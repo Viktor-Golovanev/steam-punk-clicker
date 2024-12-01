54b7b436448573b2fc419b378d0393e6e4e3e8bd
Виктор <vik.3dmodel@gmail.com>
2024-12-02 01:59:33 +0300

    Добавлен маршрукт Логина

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,13 @@ const router = express.Router()
 const Users = require('../models/User')
 const bcrypt = require('bcrypt')
 
-async function hashPassword(password) {
-	const saltRound = 10
+async function hashPassword(password, saltRound) {
 	const hash = await bcrypt.hash(password, saltRound)
 	return hash
 }
 
 router.get('/', async (req, res) => {
 	try {
-		s
 		const users = await Users.findAll()
 		res.json(users)
 	} catch (error) {
@@ -20,10 +18,13 @@ router.get('/', async (req, res) => {
 	}
 })
 
-router.post('/', async (req, res) => {
+router.post('/register', async (req, res) => {
 	try {
+		console.log('получаем объект из запроса')
 		let { username, email, password_hash, premium_account } = req.body
-		password_hash = await hashPassword(password_hash)
+		console.log('хэшируем пароль')
+		password_hash = await hashPassword(password_hash, 10)
+		console.log('записываем пользователя в базу данных')
 		const newUser = await Users.create({
 			username,
 			email,
@@ -34,6 +35,32 @@ router.post('/', async (req, res) => {
 	} catch (error) {
 		console.log({ error: 'Ошибка при создании пользователя' })
 		res.status(500).json({ error: 'Ошибка при создании пользователя' })
+	}
+})
+
+router.post('/login', async (req, res) => {
+	try {
+		const user = await Users.findOne({
+			where: {
+				username: req.body.username,
+			},
+		})
+
+		if (!user) {
+			console.log('Пользователя с таким ником нет')
+			res.status(404).send('Пользователя с таким ником нет')
+			return
+		}
+
+		const inValid = await bcrypt.compare(req.body.password, user.password_hash)
+		if (!inValid) {
+			console.log('Не верный пароль')
+			res.send('Не верный пароль')
+			return
+		}
+		res.status(200).json(user)
+	} catch (error) {
+		console.log(error)
 	}
 })
 
